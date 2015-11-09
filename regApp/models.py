50f3adf70e95acbd9cbca0876e1c51974fa0d063
sandro tm <sandromail@gmail.com>
2015-11-09 21:00:14 +0400

    front-validation

--- regApp/models.py
+++ regApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-# Create your models here.
-class Region(models.Model):
+
+'''class Region(models.Model):
     name = models.CharField(max_length = 95)
     
 
@@ -10,7 +10,7 @@ class District(models.Model):
     region = models.ForeignKey(Region)
     
     
-'''class Phone(models.Model):
+class Phone(models.Model):
     value = models.CharField(max_length = 15) 
     type = models.CharField(
         choices = (
@@ -27,10 +27,20 @@ class Member(models.Model):
     # identity
     first_name = models.CharField(max_length = 95, verbose_name = 'სახელი')
     last_name = models.CharField(max_length = 95, verbose_name = 'გვარი')
-    personal_id = models.CharField(max_length = 12, verbose_name = 'პირადი ნომერი')    
+    personal_id = models.CharField(max_length = 12, verbose_name = 'პირადი ნომერი') 
+    email_confirm = models.CharField(max_length = 95, 
+        choices = (
+            ('fail', 'ვერ გაიგზავნა'),
+            ('unconfirmed', 'გაიგზავნა, მაგრამ არ დაუდასტურებია'),
+            ('confirmed', 'დაადასტურა'),
+        ),
+        default = 'fail',
+    )
+    member_confirm_id = models.CharField(max_length = 12, unique = True, 
+        verbose_name = 'წევრის უნიკალური იდენტიფიკატორი', blank = True, null = True)
     # contact
     fb_id = models.CharField(max_length = 190, blank = True, null = True)
-    email = models.EmailField(verbose_name = 'ელ-ფოსტა', blank = True, null = True)
+    email = models.EmailField(verbose_name = 'ელ-ფოსტა')
     phone_type = models.CharField(
         choices = (
             ('mobile', 'მობილური'), 
@@ -40,7 +50,7 @@ class Member(models.Model):
         max_length = 12,
         default = 'mobile',
     )
-    phone = models.CharField(max_length = 15)   
+    phone = models.CharField(max_length = 15, blank = True, null = True)   
     # location
     district = models.CharField(max_length = 95, verbose_name = 'რაიონი / რეგიონი')    
     settlement = models.CharField(max_length = 95, verbose_name = 'ქალაქი ან სოფელი')
@@ -51,8 +61,15 @@ class Member(models.Model):
         blank = True, null = True)
     position = models.CharField(max_length = 190, verbose_name = 'თანამდებობა', 
         blank = True, null = True)
-    '''sex = models.CharField(choices = (('male', 'მამრობითი'), ('female', 'მდედრობითი')),
-        max_length = 15) # should we limit to these 2?'''
+    sex = models.CharField(max_length = 15, 
+        choices = (
+            ('unknown', 'უცნობი'),
+            ('male', 'მამრობითი'), 
+            ('female', 'მდედრობითი'),
+            ('other', 'სხვა'),
+        ),
+        default = 'unknown'
+    )
     
     def __str__(self):
         return self.first_name + ' ' + self.last_name
